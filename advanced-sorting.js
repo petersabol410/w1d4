c8b65e9a264d1d55b658832aa07edf9c255c15b3
petersabol410 <peter.sabol8@gmail.com>
2018-01-18 22:30:46 +0000

    advanced sorting code

---
+++ advanced-sorting.js
@@ -0,0 +1,31 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+  students.sort(function(a, b) {
+    if (b.name < a.name) {
+      return 1;
+    }
+      return b.age - a.age;
+  });
+
+  console.log(students);
+
+
+
+
+
+//   function compare(a, b) {
+//   if (a is less than b by some ordering criterion) {
+//     return -1;
+//   }
+//   if (a is greater than b by the ordering criterion) {
+//     return 1;
+//   }
+//   // a must be equal to b
+//   return 0;
+// }
+
